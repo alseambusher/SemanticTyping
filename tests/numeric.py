f43331d591d6a404e4117456e3e99953dc20d80c
alseambusher <alseambusher@gmail.com>
2015-12-13 13:33:50 -0800

    camel cases to standard format

--- tests/numeric.py
+++ tests/numeric.py
@@ -3,28 +3,29 @@ from pyspark.mllib.stat import Statistics
 import random
 from scipy.stats import mannwhitneyu, f_oneway, ks_2samp
 
-def KolmogorovSmirnovTest(trainExamples, testExamples, sc):
-    sample1 = sc.parallalize(random.sample(testExamples, 100 if len(testExamples) > 100 else len(testExamples)))
-    sample2 = sc.parallalize(random.sample(trainExamples, 100 if len(trainExamples) > 100 else len(trainExamples)))
+
+def kolmogorov_smirnov_test(train_examples, test_examples, sc):
+    sample1 = sc.parallalize(random.sample(test_examples, 100 if len(test_examples) > 100 else len(test_examples)))
+    sample2 = sc.parallalize(random.sample(train_examples, 100 if len(train_examples) > 100 else len(train_examples)))
     if len(sample1) > 1 and len(sample2) > 1:
         return ks_2samp(sample1, sample2)
         #return Statistics.kolmogorovSmirnovTest(sample1, "norm", 0, 1).pValue
     return 0.0
 
 
-def MannWhitneyUTest(trainExamples, testExamples, sc):
-    if len(trainExamples) > 1 and len(testExamples) > 1:
-        return mannwhitneyu(trainExamples, testExamples).pvalue
+def mann_whitney_u_test(train_examples, test_examples, sc):
+    if len(train_examples) > 1 and len(test_examples) > 1:
+        return mannwhitneyu(train_examples, test_examples).pvalue
     return 0.0
 
 
-def MannWhitneyNumTest(trainExamples, testExamples, sc):
-    return MannWhitneyUTest(trainExamples, testExamples, sc)
+def mann_whitney_num_test(train_examples, test_examples, sc):
+    return mann_whitney_u_test(train_examples, test_examples, sc)
 
 
-def AnovaTest(trainExamples, testExamples, sc):
-    if len(trainExamples) > 1 and len(testExamples) > 1:
-        return f_oneway(trainExamples, testExamples).pvalue
+def anova_test(train_examples, test_examples, sc):
+    if len(train_examples) > 1 and len(test_examples) > 1:
+        return f_oneway(train_examples, test_examples).pvalue
     return 0.0
 
 
--- tests/textual.py
+++ tests/textual.py
@@ -4,14 +4,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.spatial.distance import cosine
 
 
-def tfidf(trainExamples, testExamples):
+def tfidf(train_examples, test_examples):
     vectorizer = TfidfVectorizer()
-    data = [" ".join(trainExamples), " ".join(testExamples)]
+    data = [" ".join(train_examples), " ".join(test_examples)]
     value = vectorizer.fit(data).toarray()
     return cosine(value[0], value[1])
 
 
-def soft_tfidf(trainExamples, testExamples):
+def soft_tfidf(train_examples, test_examples):
     pass
 
 
@@ -21,12 +21,12 @@ def get_abbr_patterns(abbr):
     return []
 
 
-def abbr_test(trainExamples, testExamples):
-    # TODO if testExamples is a string, perform metadata abbr test (label thing). Else do the normal one
-    if isinstance(testExamples, str):
+def abbr_test(train_examples, test_examples):
+    # TODO if test_examples is a string, perform metadata abbr test (label thing). Else do the normal one
+    if isinstance(test_examples, str):
         pass
     else:
         pass
-    if len(testExamples) > 200:
+    if len(test_examples) > 200:
         return 0.0
 
